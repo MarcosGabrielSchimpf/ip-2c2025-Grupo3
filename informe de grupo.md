f7e06dffeb4ebd8a6654da2b8c711086d2d1f101
Foxo-Dev009 <schimpfmarcosgabriel@gmail.com>
2025-11-09 16:52:03 -0300

    algoritmo "bubble" completo
    primer algoritmo completo, junto al inicio de un archivo .md para hacer el informe

---
+++ informe de grupo.md
@@ -0,0 +1,17 @@
+Integrantes: 
+  Schimpf Marcos Gabriel 
+  Brandan Luca Leandro
+
+
+
+Informe: 
+
+Marcos Schimpf:
+La primera desicion que tuve fue la de buscar informacion de la funcionalidad de los algoritmos requeridos, mirando lo dificiles o faciles que serian de implementar a medida que el proyecto avance para luego decidir si implementar los opcionales, por medio de las siguientes paginas web:
+
+https://www.geeksforgeeks.org/dsa/bubble-sort-algorithm/
+https://www.geeksforgeeks.org/dsa/insertion-sort-algorithm/?_x_tr_sl&_x_tr_tl&_x_tr_hl
+https://www.programiz.com/dsa/selection-sort
+
+La primera dificultad fue averiguar que significaba cada variable (en sort_bubble), deduciendo que N era la longitud de la lista a ordenar, I era el numero de pasadas que tuvo la lista, y J era el indice de los 2 numeros a comparar. y actuando en step() como si fuera un ciclo "While done == False".
+Con estas fuentes, prueba y error buscando lo que funcionaba y lo que estaba mal, y mirando los resultados en el HTML dado, fue medianamente simple, aunque no facil, formar el primer algoritmo de ordenamiento sort_bubble
--- visualizador/algorithms/sort_bubble.py
+++ visualizador/algorithms/sort_bubble.py
@@ -1,23 +1,50 @@
 # Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
 
 items = []
-n = 0
-i = 0
-j = 0
+length = 0
+pasada = 0
+index = 0
 
 def init(vals):
-    global items, n, i, j
+    global items, length, pasada, index
     items = list(vals)
-    n = len(items)
-    i = 0
-    j = 0
+    length = len(items)
+    pasada = 0
+    index = 0
 
 def step():
-    # TODO:
+    global items, length, pasada, index
+
+    # Si ya terminamos todas las pasadas
+    if pasada >= length - 1:
+        return {"done": True}
+    
+    
     # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
+    aux = 0
+    swap = False    
+    a = index
+    b = index+1
+
+
+
     # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
-    # 3) Avanzar punteros (preparar el próximo paso).
-    # 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
-    #
-    # Cuando no queden pasos, devolvé {"done": True}.
-    return {"done": True}
+    if items[a] > items[b]:
+        aux = items[a]
+        items[a] = items[b]
+        items[b] = aux
+        swap = True
+
+     # 3) Avanzar punteros (preparar el próximo paso).
+    index = index+1
+    if index >= length - 1 - pasada: #length recorre toda la lista, -1 porque listas cuentan de 1 a n en vez de 0 a n-1, 
+        #y por cada pasada el numero mas grande deberia estar al final, por lo tanto lo restamos para evitar una pasada completa por posibles decenas de numeros
+        index = 0
+        pasada += 1
+
+        #reseteamos index para pasar de nuevo, aumentamos "Pasada", y damos la informacion del contrato
+
+    return {"a": a, "b": b, "swap": swap, "done": False}
+
+    
+
